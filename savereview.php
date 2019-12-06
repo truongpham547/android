b147dfe81c09426622230a546a563815184be6c4
truongpham547 <phamlamtruongag@gmail.com>
2019-12-06 13:29:39 +0700

    delete savereview

--- savereview.php
+++ savereview.php
@@ -5,27 +5,43 @@
     {
         $username=$_POST['username'];
         $idreview=$_POST['idreview'];
+        $isdel=$_POST['delete'];
         $ngayluu=date('Y-m-d H:i:s');
         require_once 'connect.php';
         
-        $sql="SELECT * FROM save_table WHERE username='$username' and idreview='$idreview'";
-        $response=$conn->query($sql);
-        $row=mysqli_fetch_assoc($response);
-        $sql="INSERT INTO save_table (username,idreview,ngayluu) VALUES ('$username','$idreview','$ngayluu')";
-
-        if($row||$conn->query($sql)===TRUE)
+        if(isset($isdel))
         {
-            if($row) 
-            $conn->query("UPDATE save_table set ngayluu='$ngayluu'");
-            $result['success']='1';
-            echo json_encode($result);
-            mysqli_close($conn);
-        }
-        
-        else{
-            $result['success']='0';
-            echo json_encode($result);
-            mysqli_close($conn);
+            $sql="DELETE FROM save_table WHERE username='$username' and idreview='$idreview'";
+            if($conn->query($sql)===TRUE)
+            {
+                $result['success']='1';
+                echo json_encode($result);
+                mysqli_close($conn);
+            }
+            else{
+                $result['success']='0';
+                echo json_encode($result);
+                mysqli_close($conn);
+            }
+        } else{
+            $sql="SELECT * FROM save_table WHERE username='$username' and idreview='$idreview'";
+            $response=$conn->query($sql);
+            $row=mysqli_fetch_assoc($response);
+            $sql="INSERT INTO save_table (username,idreview,ngayluu) VALUES ('$username','$idreview','$ngayluu')";
+
+            if($row||$conn->query($sql)===TRUE)
+            {
+                if($row) 
+                $conn->query("UPDATE save_table set ngayluu='$ngayluu'");
+                $result['success']='1';
+                echo json_encode($result);
+                mysqli_close($conn);
+            }
+            else{
+                $result['success']='0';
+                echo json_encode($result);
+                mysqli_close($conn);
+            }
         }
     }
 ?>
