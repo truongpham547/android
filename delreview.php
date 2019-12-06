2c1f38c4a5ec3f281c88f262cd2f8bcaddfb6d05
truongpham547 <phamlamtruongag@gmail.com>
2019-12-06 22:11:14 +0700

    123

--- delreview.php
+++ delreview.php
@@ -1,19 +1,32 @@
 <?php
     include 'connect.php';
+    require('jwt.php');
     if($_SERVER['REQUEST_METHOD']=='POST')
     {
-        $idreview=$_POST['idreview'];
         require_once 'connect.php';
-
-        $sql="DELETE FROM review_table WHERE id='$idreview'";
-        $name_hinhanh=$idreview.'.jpg';
-        $link='hinhanh/'.$name_hinhanh;
-        unlink($link);
-        if($conn->query($sql)===TRUE)
+        $idreview=$_POST['idreview'];
+        $username=$_POST['username'];
+        $token=$_POST['token'];
+        try{
+            $auth=JWT::decode($token, "truong pham", true);
+        } catch(Exception $e){}
+        if($auth==$username)
         {
-            $result['success']='1';
-            echo json_encode($result);
-            mysqli_close($conn);
+            $sql="DELETE FROM review_table WHERE id='$idreview'";
+            $name_hinhanh=$idreview.'.jpg';
+            $link='hinhanh/'.$name_hinhanh;
+            unlink($link);
+            if($conn->query($sql)===TRUE)
+            {
+                $result['success']='1';
+                echo json_encode($result);
+                mysqli_close($conn);
+            }
+            else{
+                $result['success']='0';
+                echo json_encode($result);
+                mysqli_close($conn);
+            }
         }
         else{
             $result['success']='0';
