6aec580983deb5bb7ae63d7d032fd47f7fe7b41d
truongpham547 <phamlamtruongag@gmail.com>
2019-12-08 16:24:31 +0700

    1

--- getreview.php
+++ getreview.php
@@ -28,34 +28,35 @@
         $row=mysqli_fetch_assoc($response);
         if($row)
         {
+			$area=$_GET['area'];
+			if($area!="TP. HCM") $area=$area.", TP. HCM";
             if(isset($_GET['username'])) 
 		{
 			$username=$_GET['username'];
-			$area=$_GET['area'];
-			if($area!="TP. HCM") $area=$area.", TP. HCM";
+			
 			if(isset($_GET['getsave']))
 				$sql="SELECT id,review_table.username,DATE_FORMAT(ngaydang, '%d/%m/%Y') ngaydang,tieude,noidung,diachi,hinhanh,rating 
-				FROM review_table inner join (select *from save_table WHERE username='$username') a on id=idreview where diachi like %'$area'% order by ngayluu desc";
+				FROM review_table inner join (select *from save_table WHERE username='$username') a on id=idreview where diachi like '%$area%' order by ngayluu desc";
 			else        
 				$sql="SELECT id,username,DATE_FORMAT(ngaydang, '%d/%m/%Y') ngaydang,tieude,noidung,diachi,hinhanh,rating 
-				FROM review_table where username='$username' and diachi like %'$area'% order by id desc";
+				FROM review_table where username='$username' and diachi like '%$area%' order by id desc";
 
 		} else
 		if(isset($_GET['search']))
 		{
 			$keyword=''.$_GET['search'].'';
 			$sql="SELECT id,username,DATE_FORMAT(ngaydang, '%d/%m/%Y') ngaydang,tieude,noidung,diachi,hinhanh,rating 
-				FROM review_table where MATCH(tieude,diachi) AGAINST('$keyword') and diachi like %'$area'%";
+				FROM review_table where MATCH(tieude,diachi) AGAINST('$keyword') and diachi like '%$area%'";
 		} 
 		else
 		if(isset($_GET['trending']))
 		{
 			$sql="SELECT id,username,DATE_FORMAT(ngaydang, '%d/%m/%Y') ngaydang,tieude,noidung,diachi,hinhanh,rating 
 				FROM review_table left join (select idreview,count(*) soluong from rate_table group by idreview) a
-				on id=idreview where diachi like %'$area'% order by soluong desc,id desc";
+				on id=idreview where diachi like '%$area%' order by soluong desc,id desc";
 		} 
 		else $sql="SELECT id,username,DATE_FORMAT(ngaydang, '%d/%m/%Y') ngaydang,tieude,noidung,diachi,hinhanh,rating 
-				FROM review_table where diachi like %'$area'% order by id desc";
+				FROM review_table where diachi like '%$area%' order by id desc";
 		$arrreview=array();
 	    $data=$conn->query($sql);
 		if($data){
